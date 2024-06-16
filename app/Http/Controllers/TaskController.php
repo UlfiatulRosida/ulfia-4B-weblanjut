7c6ec5f21c92da8901a60e49d0e54f99a44dd86a
UlfiatulRosida <ulfiarosidah52@gmail.com>
2024-06-16 13:15:16 +0700

    first commit

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,171 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\tasks;
+use Illuminate\Console\View\Components\Task as ComponentsTask;
+use Illuminate\Contracts\View\View;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Redirect;
+use Illuminate\Support\Facades\Storage;
+
+class TaskController extends Controller
+{
+    public function index() : View
+    {
+        $tasks = tasks::latest()->paginate(10);
+
+        return view('tasks.index', compact('tasks')
+        );
+    }
+    /**
+     * create
+     *
+     * @return View
+     */
+    public function create(): View
+    {
+        return view('tasks.create');
+    }
+
+    /**
+     * store
+     *
+     * @param  mixed $request
+     * @return RedirectResponse
+     */
+    public function store(Request $request): RedirectResponse
+    {
+        //validate form
+        $request->validate([
+            'image'         => 'required|image|mimes:jpeg,jpg,png|max:2048',
+            'title'         => 'required|min:5',
+            'description'   => 'required|min:10',
+            'price'         => 'required|numeric',
+            'stock'         => 'required|numeric'
+        ]);
+
+        //upload image
+        $image = $request->file('image');
+        $image->storeAs('public/tasks', $image->hashName());
+
+        //create tasks
+    tasks::create([
+            'image'         => $image->hashName(),
+            'title'         => $request->title,
+            'description'   => $request->description,
+            'price'         => $request->price,
+            'stock'         => $request->stock
+        ]);
+
+        //redirect to index
+        return redirect()->route('tasks.index')->with(['success' => 'Data Berhasil Disimpan!']);
+    }
+
+    /**
+     * show
+     *
+     * @param  mixed $id
+     * @return View
+     */
+    public function show(string $id): View
+    {
+        //get product by ID
+        $tasks = tasks::findOrFail($id);
+
+        //render view with product
+        return view('tasks.show', compact('tasks'));
+    }
+
+    /**
+     * edit
+     *
+     * @param  mixed $id
+     * @return View
+     */
+    public function edit(string $id): View
+    {
+        //get product by ID
+        $tasks = tasks::findOrFail($id);
+
+        //render view with product
+        return view('tasks.edit', compact('tasks'));
+    }
+
+     /**
+     * update
+     *
+     * @param  mixed $request
+     * @param  mixed $id
+     * @return RedirectResponse
+     */
+    public function update(Request $request, $id): RedirectResponse
+    {
+        //validate form
+        $request->validate([
+            'image'         => 'image|mimes:jpeg,jpg,png|max:2048',
+            'title'         => 'required|min:5',
+            'description'   => 'required|min:10',
+            'price'         => 'required|numeric',
+            'stock'         => 'required|numeric'
+        ]);
+
+        //get product by ID
+        $tasks = tasks::findOrFail($id);
+
+        //check if image is uploaded
+        if ($request->hasFile('image')) {
+
+            //upload new image
+            $image = $request->file('image');
+            $image->storeAs('public/tasks', $image->hashName());
+
+            //delete old image
+            Storage::delete('public/tasks/'.$tasks->image);
+
+            //update product with new image
+            $tasks->update([
+                'image'         => $image->hashName(),
+                'title'         => $request->title,
+                'description'   => $request->description,
+                'price'         => $request->price,
+                'stock'         => $request->stock
+            ]);
+
+        } else {
+
+            //update product without image
+            $tasks->update([
+                'title'         => $request->title,
+                'description'   => $request->description,
+                'price'         => $request->price,
+                'stock'         => $request->stock
+            ]);
+        }
+
+        //redirect to index
+        return redirect()->route('tasks.index')->with(['success' => 'Data Berhasil Diubah!']);
+    }
+
+    /**
+     * destroy
+     *
+     * @param  mixed $id
+     * @return RedirectResponse
+     */
+    public function destroy($id): RedirectResponse
+    {
+        //get product by ID
+        $product = tasks::findOrFail($id);
+
+        //delete image
+        Storage::delete('public/products/'. $product->image);
+
+        //delete product
+        $product->delete();
+
+        //redirect to index
+        return redirect()->route('tasks.index')->with(['success' => 'Data Berhasil Dihapus!']);
+    }
+}
---
+++ resources/views/tasks/index.blade.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Data Products</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body style="background: lightgray">
+
+    <div class="container mt-5">
+        <div class="row">
+            <div class="col-md-12">
+                <div>
+                    <h3 class="text-center my-4">DATA PRODUK YANG TERSEDIA</h3>
+                    <h5 class="text-center"><a href="https://id.oriflame.com/skincare/">www.Oriflame.com</a></h5>
+                    <hr>
+                </div>
+                <div class="card border-0 shadow-sm rounded">
+                    <div class="card-body">
+                        <a href="{{ route('tasks.create') }}" class="btn btn-md btn-success mb-3">ADD PRODUCT</a>
+                        <table class="table table-bordered">
+                            <thead>
+                                <tr>
+                                    <th scope="col">IMAGE</th>
+                                    <th scope="col">TITLE</th>
+                                    <th scope="col">PRICE</th>
+                                    <th scope="col">STOCK</th>
+                                    <th scope="col" style="width: 20%">ACTIONS</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @forelse ($tasks as $tasks)
+                                    <tr>
+                                        <td class="text-center">
+                                            <img src="{{ asset('/storage/tasks/'.$tasks->image) }}" class="rounded" style="width: 150px">
+                                        </td>
+                                        <td>{{ $tasks->title }}</td>
+                                        <td>{{ "Rp " . number_format($tasks->price,2,',','.') }}</td>
+                                        <td>{{ $tasks->stock }}</td>
+                                        <td class="text-center">
+                                            <form onsubmit="return confirm('Apakah Anda Yakin ?');" action="{{ route('tasks.destroy', $tasks->id) }}" method="POST">
+                                                <a href="{{ route('tasks.show', $tasks->id) }}" class="btn btn-sm btn-dark">SHOW</a>
+                                                <a href="{{ route('tasks.edit', $tasks->id) }}" class="btn btn-sm btn-primary">EDIT</a>
+                                                @csrf
+                                                @method('DELETE')
+                                                <button type="submit" class="btn btn-sm btn-danger">HAPUS</button>
+                                            </form>
+                                        </td>
+                                    </tr>
+                                @empty
+                                    <div class="alert alert-danger">
+                                        Data Products belum Tersedia.
+                                    </div>
+                                @endforelse
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+
+    <script>
+        //message with sweetalert
+        (session('success'))
+            Swal.fire({
+                icon: "success",
+                title: "BERHASIL",
+                text: "{{ session('success') }}",
+                showConfirmButton: false,
+                timer: 2000
+            });
+        (session('error'))
+            Swal.fire({
+                icon: "error",
+                title: "GAGAL!",
+                text: "{{ session('error') }}",
+                showConfirmButton: false,
+                timer: 2000
+            });
+    </script>
+
+</body>
+</html>
